17ac3e0b180b6064af70f497be14071fe5c77d7e
hofq <jona.kieltsch@bat-bs.com>
2024-10-07 15:48:12 +0200

    move Migration to seperate file

--- db/database.go
+++ db/database.go
@@ -1,14 +1,11 @@
 package database
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 
-	"ariga.io/atlas-go-sdk/atlasexec"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
@@ -54,41 +51,6 @@ func NewDB() *Database {
 	}
 	return &d
 
-}
-
-func (d *Database) Migrate() {
-	// Define the execution context, supplying a migration directory
-	// and potentially an `atlas.hcl` configuration file using `atlasexec.WithHCL`.
-	workdir, err := atlasexec.NewWorkingDir(
-		atlasexec.WithMigrations(
-			os.DirFS("db/migrations"),
-		),
-	)
-	if err != nil {
-		log.Fatalf("failed to load working directory: %v", err)
-	}
-	// atlasexec works on a temporary directory, so we need to close it
-	defer workdir.Close()
-
-	// Initialize the client.
-	client, err := atlasexec.NewClient(workdir.Path(), "atlas")
-	if err != nil {
-		log.Fatalf("failed to initialize client: %v", err)
-	}
-
-	var atlasurl string
-
-	var re = regexp.MustCompile(`(postgresql://)([^?]*)(?:\?(.*))?`)
-	atlasurl = re.ReplaceAllString(databasePath, `postgres://$2?search_path=public&$3`)
-
-	// Run `atlas migrate apply` on a PSQL database
-	res, err := client.MigrateApply(context.Background(), &atlasexec.MigrateApplyParams{
-		URL: atlasurl,
-	})
-	if err != nil {
-		log.Fatalf("failed to apply migrations: %v", err)
-	}
-	fmt.Printf("Applied %d migrations\n", len(res.Applied))
 }
 
 // func (d *Database) CheckAndCreateUser(sub string) string {
